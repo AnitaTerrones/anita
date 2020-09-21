d56e98c63043dd05e0caeadf4096b082db076f6a
AnitaTerrones <anitaterrones0421@gmail.com>
2020-09-21 14:16:40 -0500

    Initial Commit

---
+++ T1-Diars/Models/Persona.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace T1_Diars.Models
+{
+    public class Persona
+    {
+        [Required(ErrorMessage = "El campo Nombre es obligatorio")]
+        public int Nombre { get; set; }
+        
+
+        [Required(ErrorMessage = "El campo Apellidos es obligatorio")]
+        public string Apellidos { get; set; }
+
+
+        [Required(ErrorMessage = "El campo F.Nacimeinto es obligatorio")]
+        public string FNacimiento { get; set; }
+
+
+        [Required(ErrorMessage = "El campo DNI es obligatorio")]
+        public string DNI { get; set; }
+
+
+        [Required(ErrorMessage = "El campo Genero es obligatorio")]
+        public decimal Genero { get; set; }
+
+
+        [Required(ErrorMessage = "El campo Ciudad es obligatorio")]
+        public decimal Ciudad { get; set; }
+
+
+        [Required(ErrorMessage = "El campo Direccion es obligatorio")]
+        public string Direccion { get; set; }
+
+
+        [Required(ErrorMessage = "El campo Correo es obligatorio")]
+        public decimal Correo { get; set; }
+
+
+        [Required(ErrorMessage = "El campo Usuario es obligatorio")]
+        public decimal Username { get; set; }
+
+
+        [Required(ErrorMessage = "El campo Password es obligatorio")]
+        public decimal Password { get; set; }
+
+
+    }
+
+}
---
+++ T1-Diars/Models/PersonaContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using T1_Diars.Models.Maps;
+
+namespace T1_Diars.Models
+{
+    public class PersonaContext : DbContext 
+    {
+        public DbSet<Persona> Personas { get; set; }
+
+        public PersonaContext(DbContextOptions<PersonaContext> options)
+            : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfiguration(new PersonaMap());
+        }
+    }
+}
